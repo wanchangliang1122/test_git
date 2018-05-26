c4f6f7ac90c74b7418299b2d91cc023e03f7087c
D8Ge <burnedfrog@163.com>
2018-05-26 14:18:29 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by yeyongping on 2018/5/26.
+//  Copyright © 2018年 wanchangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by yeyongping on 2018/5/26.
+//  Copyright © 2018年 wanchangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
